84b7801379cc37bd68505ea5a2c150b984cd0584
Obrador, Jona Rae <jrobrador@netsuite.com>
2021-06-12 03:20:37 +0800

    Fix incorrect project folder returned by _getProjectFolder()

--- packages/unit-testing/jest-configuration/SuiteCloudJestConfiguration.js
+++ packages/unit-testing/jest-configuration/SuiteCloudJestConfiguration.js
@@ -118,7 +118,7 @@ class SuiteCloudAdvancedJestConfiguration {
 				}
 			}
 		}
-		return path.join(process.cwd(), projectFolder);
+		return path.join(process.cwd()/*, projectFolder*/);
 	}
 
 	_getSuiteScriptFolderPath() {
